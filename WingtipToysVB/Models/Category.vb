05096e293829bb9ae66a1ed8d2b11257a5871d9a
Michael Sheehan <mks85311@gmail.com>
2022-05-20 03:41:41 -0500

    Created data access layer

---
+++ WingtipToysVB/Models/Category.vb
@@ -0,0 +1,15 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class Category
+    <ScaffoldColumn(False)>
+    Public Property CategoryID() As Integer
+
+    <Required>
+    <StringLength(100)>
+    <Display(Name:="Name")>
+    Public Property CategoryName() As String
+
+    <Display(Name:="Product Description")>
+    Public Property Description() As String
+    Public Overridable Property Products() As ICollection(Of Product)
+End Class
---
+++ WingtipToysVB/Models/Product.vb
@@ -0,0 +1,37 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.Data.Entity
+
+Public Class Product
+
+    <ScaffoldColumn(False)>
+    Public Property ProductID() As Integer
+
+    <Required>
+    <StringLength(100)>
+    <Display(Name:="Name")>
+    Public Property ProductName() As String
+
+    <Required>
+    <StringLength(1000)>
+    <Display(Name:="Product Description")>
+    Public Property Description() As String
+    Public Property ImagePath() As String
+
+    <Display(Name:="Price")>
+    Public Property UnitPrice() As Double
+    Public Property CategoryID() As Integer
+
+    <ForeignKey("CategoryID")>
+    Public Property CategoryRef() As Category
+End Class
+
+Public Class ProductContext
+    Inherits DbContext
+    Public Property Products() As DbSet(Of Product)
+    Public Property Categories() As DbSet(Of Category)
+
+    Public Sub New()
+        MyBase.New("WingtipToys")
+    End Sub
+End Class
